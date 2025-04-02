d8399d26115e118a7a9c43ba71b048971d359e20
sumitsheoran521 <sumitsheoran521@gmail.com>
2025-04-02 17:55:00 +0530

    user

--- src/models/user.js
+++ src/models/user.js
@@ -5,21 +5,36 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   firstName: {
     type: String,
+    required: true,
   },
   lastName: {
     type: String,
   },
   emailId: {
     type: String,
+    required: true,
+    unique: true,
   },
   password: {
     type: String,
+    required: true,
   },
   age: {
     type: Number,
   },
   gender: {
     type: String,
+  },
+  photoURL: {
+    type: String,
+    default: "https://geographyandyou.com/images/user-profile.png",
+  },
+  about: {
+    type: String,
+    default: "This is a default about of the user!",
+  },
+  skills: {
+    type: [String],
   },
 });
 
